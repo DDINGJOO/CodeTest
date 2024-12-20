690fe46973dfcc5c96718fa4f3b52585dd073f25
DDINGJOO <ddingsha9@teambind.co.kr>
2024-12-20 19:54:03 +0900

    [level 2] Title: 짝지어 제거하기, Time: 53.33 ms, Memory: 62.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,63 @@
+import java.util.*;
+class Solution
+{
+    public int solution(String s)
+    {
+  
+        /*
+        Given
+        String : b a a b a a
+        
+        When
+        
+        for : 
+            if : ~Stack.isEmpty()
+            문자열 i != Stack.peek 
+                Stack.push
+            
+            문자열 i == Stack.peek
+                Stack.pop
+            i+1
+        
+        Then 
+        return : 
+            문자열 모두 삭제 -> 1
+            문자열 남아 있음 ->0
+        
+        */
+        
+        
+        
+        Stack<Character> st = new Stack<>();
+        for(char ch : s.toCharArray())
+        {
+            if(st.isEmpty())
+            {
+                st.push(ch);
+            }
+            
+            else//Not Empty
+            {
+                if(ch == st.peek()) // 처리후 이전 문자 == 내 문자
+                {
+                    st.pop();
+                }
+                else
+                {
+                    st.push(ch);
+                }
+            }
+        }
+        
+        
+        if(st.isEmpty())
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    
+    }
+}
