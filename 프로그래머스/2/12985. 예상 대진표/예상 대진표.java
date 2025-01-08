58432e85da4530ed2e70bc40000676aa0ac108f6
DDINGJOO <ddingsha9@teambind.co.kr>
2025-01-08 14:09:15 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 84.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,21 @@
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {
+        int count =0;
+        
+        while(a != b)
+        {
+            if( a%2 !=0){a +=1;}
+            if( b%2 !=0){b +=1;}
+            a/=2;
+            b/=2;
+            count++;
+       }
+        return count;
+        
+    }
+    
+    
+
+}
