17ff71670d09595a211912f0c37a0a64935dc6d3
DDINGJOO <ddingsha9@teambind.co.kr>
2025-02-19 00:24:09 +0900

    [Bronze V] Title: 꼬마 정민, Time: 176 ms, Memory: 17612 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                      :::    :::    :::     */
+/*   Problem Number: 11382                             :+:    :+:      :+:    */
+/*                                                    +:+    +:+        +:+   */
+/*   By: soh1001 <boj.kr/u/soh1001>                  +#+    +#+          +#+  */
+/*                                                  +#+      +#+        +#+   */
+/*   https://boj.kr/11382                          #+#        #+#      #+#    */
+/*   Solved: 2025/02/19 00:02:16 by soh1001       ###          ###   ##.kr    */
+/*                                                                            */
+/* ************************************************************************** */
+
+import java.util.Scanner;
+
+
+public class Main{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        long c = sc.nextLong();
+
+        System.out.println(a+b+c);
+        
+    }
+}
