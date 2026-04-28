d487c7a96c2c7ca5b8938086ac014673138af56c
ddingjoo <ddingsha9@teambind.co.kr>
2026-04-28 22:51:06 +0900

    Add LeetCode 155 Min Stack solution and explanation

---
+++ 리트코드/116_ Populating Next Right Pointers in Each Node/Solution.java
@@ -0,0 +1,34 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        for (Node head = root; head != null && head.left != null; head = head.left) {
+            for (Node node = head; node != null; node = node.next) {
+                node.left.next = node.right;
+                node.right.next = node.next == null ? null : node.next.left;
+            }
+        }
+        return root;
+    }
+}
